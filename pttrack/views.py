f28c43851ac8fd3df841e1397c2cb5170eb5ccb9
Justin R. Porter <justinrporter@gmail.com>
2015-05-30 17:08:05 -0500

    Include HttpResponseRedirect in views.py

--- pttrack/views.py
+++ pttrack/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.from django.http import HttpResponse
 
