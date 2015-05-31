7cbda492dcb3254eec0751101f04e48148f0773c
Justin R. Porter <justinrporter@gmail.com>
2015-05-31 12:17:57 -0500

    Rebuild database for new person-based model.

--- pttrack/migrations/0001_initial.py
+++ pttrack/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 import pttrack.models
 
 
@@ -15,7 +16,7 @@ class Migration(migrations.Migration):
             name='ClinicDate',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('clinic_type', models.CharField(default=b'SATUR', max_length=5, choices=[(b'SATUR', b'Saturday Basic'), (b'PSYCH', b'Depression & Anxiety Specialty'), (b'ORTHO', b'Muscle and Joint Pain Specialty'), (b'DERMA', b'Dermatology Specialty')])),
+                ('clinic_type', models.CharField(default=b'BASIC', max_length=5, choices=[(b'BASIC', b'(Saturday) Basic Care Clinic'), (b'PSYCH', b'Depression & Anxiety Specialty'), (b'ORTHO', b'Muscle and Joint Pain Specialty'), (b'DERMA', b'Dermatology Specialty')])),
                 ('date', models.DateField()),
                 ('gcal_id', models.CharField(max_length=50)),
             ],
@@ -33,28 +34,14 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Patient',
+            name='Person',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('first_name', models.CharField(max_length=100)),
                 ('last_name', models.CharField(max_length=100)),
                 ('middle_name', models.CharField(max_length=100)),
-                ('address', models.CharField(max_length=200)),
-                ('city', models.CharField(default=b'St. Louis', max_length=50)),
-                ('state', models.CharField(default=b'MO', max_length=2)),
-                ('zip_code', models.CharField(max_length=5, validators=[pttrack.models.validate_zip])),
-                ('dob', models.DateField()),
-                ('ethnicity', models.CharField(max_length=50)),
                 ('phone', models.CharField(max_length=50)),
                 ('gender', models.CharField(max_length=1, choices=[(b'M', b'Male'), (b'F', b'Female'), (b'O', b'Other')])),
-                ('comp_status', models.CharField(max_length=4, choices=[(b'LTFO', b'Filed - Lost to follow up'), (b'COMP', b'Filed - encounter completed'), (b'BIN', b'In WashU Bin'), (b'PCP', b'PCP Referral Follow-up')])),
-                ('language', models.ForeignKey(to='pttrack.Language')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Provider',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
         ),
         migrations.CreateModel(
@@ -62,10 +49,32 @@ class Migration(migrations.Migration):
             fields=[
                 ('note_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pttrack.Note')),
                 ('note', models.TextField()),
-                ('written_date', models.DateField()),
+                ('written_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('next_action', models.DateField(blank=True)),
             ],
             bases=('pttrack.note',),
+        ),
+        migrations.CreateModel(
+            name='Patient',
+            fields=[
+                ('person_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pttrack.Person')),
+                ('address', models.CharField(max_length=200)),
+                ('city', models.CharField(default=b'St. Louis', max_length=50)),
+                ('state', models.CharField(default=b'MO', max_length=2)),
+                ('zip_code', models.CharField(max_length=5, validators=[pttrack.models.validate_zip])),
+                ('date_of_birth', models.DateField()),
+                ('ethnicity', models.CharField(max_length=50)),
+                ('comp_status', models.CharField(max_length=4, choices=[(b'LTFO', b'Filed - Lost to follow up'), (b'COMP', b'Filed - encounter completed'), (b'BIN', b'In WashU Bin'), (b'PCP', b'PCP Referral Follow-up')])),
+                ('language', models.ForeignKey(to='pttrack.Language')),
+            ],
+            bases=('pttrack.person',),
+        ),
+        migrations.CreateModel(
+            name='Provider',
+            fields=[
+                ('person_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pttrack.Person')),
+            ],
+            bases=('pttrack.person',),
         ),
         migrations.CreateModel(
             name='Workup',
@@ -89,7 +98,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='note',
             name='author',
-            field=models.ForeignKey(to='pttrack.Provider'),
+            field=models.ForeignKey(to='pttrack.Provider', blank=True),
         ),
         migrations.AddField(
             model_name='note',
--- pttrack/migrations/0002_auto_20150531_1717.py
+++ pttrack/migrations/0002_auto_20150531_1717.py
@@ -2,19 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.utils.timezone
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pttrack', '0004_auto_20150531_1525'),
+        ('pttrack', '0001_initial'),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='followup',
-            name='written_date',
-            field=models.DateTimeField(default=django.utils.timezone.now),
+            model_name='person',
+            name='middle_name',
+            field=models.CharField(max_length=100, blank=True),
         ),
     ]
