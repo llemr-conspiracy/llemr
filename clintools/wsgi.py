a1d9247e0d72a468e0fa70793501cd2e7dfec854
Justin R. Porter <justinrporter@gmail.com>
2015-07-15 14:15:23 -0500

    Update wsgi.py.

--- clintools/wsgi.py
+++ clintools/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for clintools project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/washu/.virtualenvs/osler/local/lib/python2.7/site-packages')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clintools.settings")
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/washu/clintools')
+sys.path.append('/home/washu/clintools/clintools')
 
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'clintools.settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/washu/.virtualenvs/osler/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+import django.core.wsgi
+application = django.core.wsgi.get_wsgi_application()
