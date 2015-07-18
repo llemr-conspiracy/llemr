0300bb45fb52dfaa801bb83b10f3e8316642026d
Justin R. Porter <justinrporter@gmail.com>
2015-07-18 16:52:10 -0500

    Update deployed settings with results from deploy check.

--- clintools/deployed_settings.py
+++ clintools/deployed_settings.py
@@ -6,6 +6,14 @@ ALLOWED_HOSTS = ['pttrack.snhc.wustl.edu']
 with open(os.path.join(BASE_DIR, 'secrets/secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+
 # TODO: change for deployment?
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
