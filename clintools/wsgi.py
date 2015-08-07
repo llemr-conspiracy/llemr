6e10e5677c368f2281ff125556649087161dfa17
Justin R. Porter <justinrporter@gmail.com>
2015-08-07 05:29:18 -0500

    Update wsgi.py to have imports appropriate for apache's auth of serving media files.

--- clintools/wsgi.py
+++ clintools/wsgi.py
@@ -4,6 +4,7 @@ import site
 
 # pylint: disable=invalid-name
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Add the app's directory to the PYTHONPATH
@@ -19,6 +20,8 @@ try:
     # If you have everything installed globally, it should Just Work (TM)
 
     import django.core.wsgi
+    from django.contrib.auth.handlers.modwsgi import check_password
+
     application = django.core.wsgi.get_wsgi_application()
 except ImportError:
     # If we fail to import django.core.wsgi, then we maybe don't have stuff
@@ -54,4 +57,6 @@ except ImportError:
     execfile(activate_env, dict(__file__=activate_env))
 
     import django.core.wsgi
+    from django.contrib.auth.handlers.modwsgi import check_password
+
     application = django.core.wsgi.get_wsgi_application()
