5beb97e89b1b5afe4a1ec3d1bd2a55a9f48ff100
Justin R. Porter <justinrporter@gmail.com>
2015-12-26 20:25:09 -0800

    workup app url restructuring.

--- workup/urls.py
+++ workup/urls.py
@@ -5,25 +5,24 @@ from django.views.generic import DetailView
 from . import models
 from . import views
 
-#TODO: remove workup from the front of all these URLs
 unwrapped_urlconf = [  # pylint: disable=invalid-name
-    url(r'^(?P<pt_id>[0-9]+)/workup/$',
+    url(r'^new/(?P<pt_id>[0-9]+)/$',
         views.WorkupCreate.as_view(),
         name='new-workup'),
-    url(r'^workup/(?P<pk>[0-9]+)$',
+    url(r'^(?P<pk>[0-9]+)/$',
         DetailView.as_view(model=models.Workup),
         name='workup'),
-    url(r'^workup/update/(?P<pk>[0-9]+)$',
+    url(r'^(?P<pk>[0-9]+)/update/$',
         views.WorkupUpdate.as_view(),
         name='workup-update'),
-    url(r'^workup/sign/(?P<pk>[0-9]+)$',
+    url(r'^(?P<pk>[0-9]+)/sign/$',
         views.sign_workup,
         name='workup-sign'),
-    url(r'^workup/error/(?P<pk>[0-9]+)$',
+    url(r'^(?P<pk>[0-9]+)/error/$',
         views.error_workup,
         name="workup-error"),
 
-    url(r'^clindate/(?P<pt_id>[0-9]+)/$',
+    url(r'^(?P<pt_id>[0-9]+)/clindate/$',
         views.ClinicDateCreate.as_view(),
         name="new-clindate"),
 ]
