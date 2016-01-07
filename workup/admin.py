f1e3a3aae62f7532270cee531f14219b23034296
Nakul Shah <nakul.m.shah@gmail.com>
2016-01-07 17:54:59 -0600

    Collapsed html in patient_detail and added DiagnosisType to admin panel

--- workup/admin.py
+++ workup/admin.py
@@ -3,7 +3,7 @@ from simple_history.admin import SimpleHistoryAdmin
 from . import models
 
 # Register your models here.
-for model in [models.ClinicDate, models.Workup, models.ClinicType]:
+for model in [models.ClinicDate, models.Workup, models.ClinicType, models.DiagnosisType]:
     if hasattr(model, "history"):
         admin.site.register(model, SimpleHistoryAdmin)
     else:
