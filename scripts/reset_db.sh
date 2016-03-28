b3b281d9986b24d8f754cc5f6d0555579c4e8a2e
Justin R. Porter <justinrporter@gmail.com>
2016-03-28 14:43:15 -0500

    Version control test db, update scripts not to make migrations anymore.

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -1,6 +1,6 @@
 rm db.sqlite3
 echo "yes" | python manage.py collectstatic
-if python manage.py makemigrations && python manage.py migrate; then
+if python manage.py migrate; then
     python manage.py shell < scripts/init_db.py
     python manage.py shell < scripts/debug_init_db.py
     chmod g+rxw db.sqlite3
