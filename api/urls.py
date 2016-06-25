77e931eff23eb4d47a994ba2b61bbe123331974c
jolbree <leejosephines@wustl.edu>
2016-06-25 10:19:06 -0500

    DRY api urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 
-from django.contrib.auth.decorators import login_required
-
-from pttrack.decorators import provider_required
+from pttrack.urls import url_wrap
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -13,25 +11,6 @@ unwrapped_urlpatterns = [  # pylint: disable=invalid-name
         views.PtList.as_view(),
         name='pt_list_api'),
 ]
-
-def url_wrap(urls):
-    '''
-    Wrap URLs in login_required and provider_required decorators as
-    appropriate.
-    '''
-    wrapped_urls = []
-    for u in urls:
-        if u.name in ['new-provider', 'choose-clintype']:
-            # do not wrap in full regalia
-            u._callback = login_required(u._callback)
-        elif u.name in ['about']:
-            # do not wrap at all, fully public
-            pass
-        else:
-            u._callback = provider_required(u._callback)
-
-        wrapped_urls.append(u)
-    return wrapped_urls
 
 urlpatterns = url_wrap(unwrapped_urlpatterns)
 urlpatterns = format_suffix_patterns(urlpatterns)
