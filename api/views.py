a809e972d9c7a0a6a783f6a22e03866d88e91a6a
Justin R. Porter <justinrporter@gmail.com>
2017-03-05 15:32:27 -0600

    refactor api.views

--- api/views.py
+++ api/views.py
@@ -1,10 +1,66 @@
 from django.core.exceptions import ImproperlyConfigured
 import django.utils.timezone
 
-from pttrack import models as mymodels
-from . import serializers
-from workup import models as workupmodels
 from rest_framework import generics
+
+from pttrack import models as coremodels
+from workup import models as workupmodels
+
+from . import serializers
+
+
+def active_patients_filter(qs):
+    '''Filter a queryset of patients for those that are listed as
+    active. This is used to display a subset of patients for voluneers
+    giving care on a particular clinic day (to hide the giant list of
+    pts).
+    '''
+    return qs.filter(needs_workup__exact=True).order_by('last_name')
+
+
+def active_ai_patients_filter(qs):
+    '''Filter a queryset of patients for those that have overdue action
+    items.
+    '''
+
+    ai_qs = coremodels.ActionItem.objects \
+        .filter(due_date__lte=django.utils.timezone.now().date()) \
+        .filter(completion_date=None) \
+        .select_related('patient')
+
+    print("activeai")
+
+    return coremodels.Patient.objects \
+        .filter(actionitem=ai_qs) \
+        .order_by('-actionitem__due_date')
+
+
+def inactive_ai_patients_filter(qs):
+    '''Filter a queryset of patients for those that have active action
+    items due in the future.
+    '''
+
+    future_ai_pts = coremodels.Patient.objects.filter(
+        actionitem=coremodels.ActionItem.objects
+            .filter(due_date__gt=django.utils.timezone.now().date())
+            .filter(completion_date=None)
+            .select_related('patient')
+        ).order_by('actionitem__due_date')
+
+    # # TODO: make this op a QuerySet
+    # future_ai_pts.sort(key=lambda pt: pt.inactive_action_items()[-1].due_date)
+
+    return future_ai_pts
+
+
+def unsigned_workup_patients_filter(qs):
+
+    wu_qs = workupmodels.Workup.objects \
+        .filter(signer__isnull=True) \
+        .order_by('last_name') \
+        .select_related('patient')  # optimization only
+
+    return coremodels.Patient.objects.filter(workup=wu_qs)
 
 
 class PtList(generics.ListAPIView):  # read only
@@ -19,6 +75,14 @@ class PtList(generics.ListAPIView):  # read only
         Restricts returned patients according to query params
         '''
 
+        filter_funcs = {
+            None: lambda x: x,
+            'active': active_patients_filter,
+            'ai_active': active_ai_patients_filter,
+            'ai_inactive': inactive_ai_patients_filter,
+            'unsigned_workup': unsigned_workup_patients_filter,
+        }
+
         def bylatestKey(pt): # This doesn't sort by latest time, just latest date
             latestwu = pt.latest_workup()
             if latestwu is None:
@@ -27,33 +91,18 @@ class PtList(generics.ListAPIView):  # read only
                 latestdate = latestwu.clinic_day.clinic_date
             return latestdate
 
-        queryset = mymodels.Patient.objects
+        queryset = coremodels.Patient.objects
         sort = self.request.query_params.get('sort', None)
-        list_type = self.request.query_params.get('filter', None) # var 'list_type' because 'filter' namespace is taken
+        filter_name = self.request.query_params.get('filter', None)
 
         if sort is not None:
             if str(sort) == 'latest_workup':
-                pt_list_latest = list(mymodels.Patient.objects.all())
-                pt_list_latest.sort(key = bylatestKey, reverse=True)
+                pt_list_latest = list(coremodels.Patient.objects.all())
+                pt_list_latest.sort(key=bylatestKey, reverse=True)
                 queryset = pt_list_latest
             else:
                 queryset = queryset.order_by(sort)
 
-        if list_type is not None:
-            list_type = str(list_type)
-            if list_type == 'active':
-                queryset = queryset.filter(needs_workup__exact=True).order_by('last_name')
-            elif list_type == 'ai_active':
-                ai_list_active = mymodels.ActionItem.objects.filter(due_date__lte=django.utils.timezone.now().date())
-                queryset = list(set([ai.patient for ai in ai_list_active if not ai.done()]))
-            elif list_type == 'ai_inactive':
-                ai_list_inactive = mymodels.ActionItem.objects.filter(due_date__gt=django.utils.timezone.now().date()).order_by('due_date')
-                pt_list_ai_inactive = list(set([ai.patient for ai in ai_list_inactive if not ai.done()]))
-                pt_list_ai_inactive.sort(key = lambda pt: pt.inactive_action_items()[-1].due_date)
-                queryset = pt_list_ai_inactive
-            elif list_type == 'unsigned_workup':
-                wu_list_unsigned = workupmodels.Workup.objects.filter(signer__isnull=True).select_related('patient')
-                pt_list_unsigned= list(set([wu.patient for wu in wu_list_unsigned]))
-                pt_list_unsigned.sort(key = lambda pt: pt.last_name)
-                queryset = pt_list_unsigned
+        queryset = filter_funcs[filter_name](queryset)
+
         return queryset
