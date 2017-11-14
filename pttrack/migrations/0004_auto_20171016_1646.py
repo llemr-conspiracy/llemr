e88609626aeffe2bea888d06997ed95dce53c7fe
zhouminerva <zhouminerva@gmail.com>
2017-11-14 14:41:12 -0600

    fixed the migration hopefully

--- pttrack/migrations/0004_auto_20171016_1646.py
+++ pttrack/migrations/0004_auto_20171016_1646.py
@@ -7,8 +7,12 @@ from django.db import migrations, models
 def move_case_manager(apps, schema_editor):
     Patient = apps.get_model('pttrack','Patient')
     for patient_person in Patient.objects.all():
-        patient_person.case_managers.add(patient_person.case_manager)
-        patient_person.case_managers.add(patient_person.case_manager_2)
+        try:
+            patient_person.case_managers.add(patient_person.case_manager)
+            if patient_person.case_manager_2 != None:
+                patient_person.case_managers.add(patient_person.case_manager_2)
+        except AttributeError:
+            pass
 
 
 class Migration(migrations.Migration):
