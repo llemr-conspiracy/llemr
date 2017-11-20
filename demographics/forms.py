25f9a622b323cb68d2f864492896d88e8f8a215b
Justin R. Porter <justinrporter@gmail.com>
2017-11-19 16:16:23 -0800

    Remove cruft from demographics/forms.py

--- demographics/forms.py
+++ demographics/forms.py
@@ -1,29 +1,16 @@
-from django.forms import ModelForm, ChoiceField, NullBooleanField
+from django.forms import ModelForm
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Fieldset, Div, Field
-from crispy_forms.bootstrap import TabHolder, Tab
+from crispy_forms.layout import Submit, Layout, Fieldset
 
 from . import models
 
 
 class DemographicsForm(ModelForm):
 
-    # my_choices = (
-    #     (None, 'Not Answered'),
-    #     (True, 'Yes'),
-    #     (False, 'No'),
-    #     )
-
-    # has_insurance = NullBooleanField(choices=my_choices, required=False)
-    # lives_alone = ChoiceField(choices=my_choices, required=False)
-    # currently_employed = ChoiceField(choices=my_choices, required=False)
-    # ER_visit_last_year = ChoiceField(choices=my_choices, required=False)
-
     class Meta:
         model = models.Demographics
-        exclude = ['patient', 'creation_date' ] #,'has_insurance', 'lives_alone',
-                   #'currently_employed', 'ER_visit_last_year']
+        exclude = ['patient', 'creation_date']
 
     def __init__(self, *args, **kwargs):
         super(DemographicsForm, self).__init__(*args, **kwargs)
@@ -36,21 +23,20 @@ class DemographicsForm(ModelForm):
 
         self.helper.layout = Layout(
                 Fieldset('Medical',
-                    'has_insurance',
-                    'ER_visit_last_year',
-                    'last_date_physician_visit',
-                    'chronic_condition'),
+                         'has_insurance',
+                         'ER_visit_last_year',
+                         'last_date_physician_visit',
+                         'chronic_condition'),
                 Fieldset('Social',
-                    'lives_alone',
-                    'dependents',
-                    'resource_access',
-                    'transportation'),
+                         'lives_alone',
+                         'dependents',
+                         'resource_access',
+                         'transportation'),
                 Fieldset('Employment',
-                    'currently_employed',
-                    'education_level',
-                    'work_status',
-                    'annual_income',
-                    )
+                         'currently_employed',
+                         'education_level',
+                         'work_status',
+                         'annual_income')
         )
 
         self.helper.add_input(Submit('submit', 'Submit'))
