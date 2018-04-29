llemr-conspiracy/llemr

commit 531297b0303452859655d5157f449fe58fde1c52

the call to wu_set.count() hits the db an additional time. first() (after we order_by) will return the first or None

pttrack/models.py
@@ -295,10 +295,7 @@ class Patient(Person):
 
     def latest_workup(self):
         wu_set = self.workup_set
-        if wu_set.count() == 0:
-            return None
-        else:
-            return wu_set.latest(field_name="clinic_day__clinic_date")
+        wu_set.order_by("clinic_day__clinic_date").first()
 
     def notes(self):
         '''Returns a list of all the notes (workups and followups) associated

commit 1f18debaaa72da283bfdf56339a222013f6cbcc1

fix bug caused in 531297b

pttrack/models.py
@@ -312,7 +312,7 @@ class Patient(Person):
 
     def latest_workup(self):
         wu_set = self.workup_set
-        wu_set.order_by("clinic_day__clinic_date").first()
+        return wu_set.order_by("clinic_day__clinic_date").first()
 
     def notes(self):
         '''Returns a list of all the notes (workups and followups) associated