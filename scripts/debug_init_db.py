ae10340602252ca66cbaa497277205e6c29edd95
Lucas Thomas <robertlucasthomas@gmail.com>
2018-08-19 17:50:01 -0500

    Add files via upload

--- scripts/debug_init_db.py
+++ scripts/debug_init_db.py
@@ -9,10 +9,13 @@ script.
 
 from django.contrib.auth.models import User
 from pttrack import models
-from 
 from datetime import date
-
+import pandas
+import random
+import numpy
 # pylint: disable=invalid-name
+
+numpy.warnings.filterwarnings('ignore')
 
 user = User.objects.create_superuser('jrporter', 'justinrporter@wustl.edu',
                                      'password')
@@ -25,6 +28,27 @@ user = User.objects.create_user('rjain', 'jainr@wusm.wustl.edu',
 user.first_name = "Radhika"
 user.last_name = "Jain"
 user.save()
+
+
+df = pandas.read_csv("TestDatabaseFiller.csv")
+malenames = df.Male_First_Name
+femalesnames = df.Female_First_Name
+lastnames = df.Last_Name
+streetnames = df.Street_Name
+for i in xrange(0,100):
+      a = models.Patient(first_name=malenames[random.randint(0,99)],
+                        middle_name="",
+                        last_name=lastnames[random.randint(0,399)],
+                        address=streetnames[random.randint(0,499)],
+                        zip_code=str(random.randint(63101,63110)),
+                        date_of_birth=date(year=random.randint(1950,2000),month=random.randint(1,12),day=random.randint(1,28)),
+                        phone="314-555-"+str(random.randint(1000,9999)),
+                        gender=models.Gender.objects.all()[0])
+      a.save()
+
+
+
+
 
 p = models.Patient(first_name="Tommy",
                    middle_name="Lee",
