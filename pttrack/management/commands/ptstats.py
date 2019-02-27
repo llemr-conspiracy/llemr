02938f38328f43d58bc3374ed9b2e8a49978e89e
R. Lucas Thomas III <robertlucasthomas@gmail.com>
2019-02-26 21:50:43 -0600

    Update ptstats to not do mapping stuff.

--- pttrack/management/commands/ptstats.py
+++ pttrack/management/commands/ptstats.py
@@ -4,9 +4,8 @@ from django.core.management.base import BaseCommand, CommandError
 import django.utils.timezone
 from django.utils.timezone import now
 from pttrack.models import Patient
+from demographics.models import Demographics
 from workup.models import DiagnosisType
-import geopy
-import gmplot
 import numpy
 import pandas
 
@@ -17,9 +16,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         #Dictionary of the fields we want to pull for each patient:
-        meta = {'filename': 'ptstats.csv','classname': Patient,'fields': ['id', 'address', 'city', 'state', 'zip_code', 'country']}
+        meta = {'filename': 'ptstats.csv','classname': Patient,'fields': ['id', 'date_of_birth', 'gender', 'address', 'city', 'state', 'zip_code', 'country']}
         self._write_csv(**meta)
-        self.genmap(meta['filename'])
+#        self.genmap(meta['filename'])
 
     def _write_csv(self, filename, classname, fields):
         with open(filename, 'w+') as f:
@@ -31,6 +30,8 @@ class Command(BaseCommand):
             #All other fields we want to pull for each note:
             workupfields = ['attending', 'chief_complaint', 'diagnosis']
 
+            #Demographic fields we want to pull:
+            demographicfields = ['transportation']
 
             dx_cats = list(DiagnosisType.objects.all())
             
@@ -39,7 +40,8 @@ class Command(BaseCommand):
                 fields +
                 workupdatefields +
                 workupfields + 
-                ['dx_%s' % unicode(dx).lower() for dx in dx_cats])
+                ['dx_%s' % unicode(dx).lower() for dx in dx_cats] +
+                demographicfields)
 
             #Writes the rows of data
             for obj in classname.objects.all():
@@ -55,32 +57,14 @@ class Command(BaseCommand):
                     for dx in lastworkup.diagnosis_categories.all():
                         dx_v[dx_cats.index(dx)] = 1
                     row.extend(dx_v)
+                
+                try:
+                    dem = obj.demographics()
+                    if dem != None:
+                        for demprop in demographicfields:
+                            row.append(unicode(getattr(dem, demprop)))
+                except Demographics.DoesNotExist:
+                    row.append(None)
                 writer.writerow(row)
 
         print 'Data written to %s' % filename
-
-    def genmap(self, filename):
-        geolocator = geopy.geocoders.Nominatim(user_agent="cori")
-        gmap = gmplot.gmplot.GoogleMapPlotter.from_geocode("St. Louis MO")
-        heatmap = gmplot.gmplot.GoogleMapPlotter.from_geocode("St. Louis MO")
-        df = pandas.read_csv(filename)
-        addresslst = df.address
-        citylst = df.city
-        statelst = df.state
-        geodata = []
-        for i in xrange(0,len(addresslst)):
-            newadd = addresslst[i] + ', ' + citylst[i] + ', ' + statelst[i]
-            geodata.append(newadd)
-        coordslst = []
-        for geodatum in geodata:
-            location = geolocator.geocode(geodatum)
-            if location != None:
-                coords = ((location.latitude, location.longitude))
-                coordslst.append(coords)
-        latslst,longslst = zip(*coordslst)
-        for z in xrange(0,len(latslst)):
-            gmap.marker(latslst[z],longslst[z],'cornflowerblue')
-        gmap.draw("map.html")
-        heatmap.heatmap(latslst,longslst)
-        heatmap.draw("heatmap.html")
-        print "Maps of patient address data generated as 'map.html' and 'heatmap.html'"
