9b2325c6e5f0864a7970e807b11d8a6c421e38fd
Justin R. Porter <justinrporter@gmail.com>
2019-02-27 11:40:23 -0600

    Appointment no show (#186)
    * Add appointment noshow/arrived feature, improve css in appointment list
    * Other updates to appointment.

--- appointment/models.py
+++ appointment/models.py
@@ -7,6 +7,14 @@ from pttrack.models import Note
 from simple_history.models import HistoricalRecords
 
 
+def generate_default_appointment_time():
+    return now().replace(
+        hour=settings.OSLER_DEFAULT_APPOINTMENT_HOUR,
+        minute=0,
+        second=0,
+        microsecond=0)
+
+
 class Appointment(Note):
 
     PSYCH_NIGHT = 'PSYCH_NIGHT'
@@ -23,17 +31,30 @@ class Appointment(Note):
     clindate = models.DateField(verbose_name="Appointment Date")
     clintime = models.TimeField(
         verbose_name="Time of Appointment",
-        default=now().replace(
-            hour=settings.OSLER_DEFAULT_APPOINTMENT_HOUR,
-            minute=0,
-            second=0,
-            microsecond=0))
+        default=generate_default_appointment_time)
     appointment_type = models.CharField(
         max_length=15, choices=APPOINTMENT_TYPES,
         verbose_name='Appointment Type', default=CHRONIC_CARE)
     comment = models.TextField(
         help_text="What should happen at this appointment?")
+
+    pt_showed = models.NullBooleanField(
+        verbose_name="Patient Showed",
+        blank=True, help_text="Did the patient come to this appointment?")
+
     history = HistoricalRecords()
+
+    def __unicode__(self):
+        return "Appointment ({type}) for {name} on {date}".format(
+            type=self.verbose_appointment_type(),
+            name=self.patient.name(),
+            date=str(self.clindate))
+
+    def verbose_appointment_type(self):
+        appointment_type_index = list(zip(*self.APPOINTMENT_TYPES))[0].index(
+            self.appointment_type)
+
+        return self.APPOINTMENT_TYPES[appointment_type_index][1]
 
     def clean(self):
         ids = Appointment.objects.filter(clindate=self.clindate).\
--- appointment/test_views.py
+++ appointment/test_views.py
@@ -1,3 +1,6 @@
+import re
+from datetime import timedelta
+
 from django.test import TestCase
 from django.utils.timezone import now
 from django.core.urlresolvers import reverse
@@ -65,3 +68,108 @@ class TestAppointmentViews(TestCase):
                                    (reverse("appointment-new"), now().date()))
         self.assertEqual(response.context['form'].initial['clindate'],
                          now().date().strftime("%Y-%m-%d"))
+
+    def test_mark_noshow(self):
+        self.assertEqual(self.apt.pt_showed, None)
+        self.assertEqual(models.Appointment.objects.count(), 1)
+
+        response = self.client.get(reverse("appointment-mark-no-show",
+                                           args=(self.apt.pk,)))
+
+        self.assertRedirects(response, reverse('appointment-list'))
+
+        self.assertEqual(models.Appointment.objects.count(), 1)
+        self.assertEqual(models.Appointment.objects.first().pt_showed, False)
+
+        response = self.client.get('appointment-list')
+        # one 'mark as noshow' link should be gone now
+        noshow_links = re.findall(
+            re.escape('href="/appointment/') +
+            r'[0-9]+' +
+            re.escape('/noshow'),
+            response.content)
+        self.assertEqual(len(noshow_links), 0)
+
+    def test_mark_arrived(self):
+        self.assertEqual(self.apt.pt_showed, None)
+        self.assertEqual(models.Appointment.objects.count(), 1)
+
+        response = self.client.get(reverse("appointment-mark-arrived",
+                                           args=(self.apt.pk,)))
+        self.assertRedirects(response, reverse('appointment-list'))
+
+        self.assertEqual(models.Appointment.objects.count(), 1)
+        self.assertEqual(models.Appointment.objects.first().pt_showed, True)
+
+        response = self.client.get('appointment-list')
+        # one 'mark as arrived' link should be gone now
+        arrived_links = re.findall(
+            re.escape('href="/appointment/') +
+            r'[0-9]+' +
+            re.escape('/arrived'),
+            response.content)
+
+        self.assertEqual(len(arrived_links), 0)
+
+    def test_first_apt_is_today(self):
+
+        apts = []
+        for datedelta in [-1, 1]:
+            date = now().date() - timedelta(days=datedelta)
+
+            apts.append(models.Appointment.objects.create(
+                comment='test this stuff',
+                clindate=date,
+                clintime=time(9, 0),
+                appointment_type='PSYCH_NIGHT',
+                author=Provider.objects.first(),
+                author_type=ProviderType.objects.filter(
+                    signs_charts=False).first(),
+                patient=Patient.objects.first()))
+
+        # three appointments should exist, total
+        self.assertEqual(models.Appointment.objects.count(), 3)
+
+        response = self.client.get(reverse("appointment-list"), follow=True)
+        self.assertTemplateUsed('appointment/appointment_list.html')
+
+        with open('tmp.html', 'w') as f:
+            f.write(response.content)
+
+        # only two panels should appear, since one apt is in the past and
+        # two fall on the same day (today)
+        for i in range(2):
+            self.assertContains(
+                response, 'appointment-panel-%s' % i)
+            self.assertContains(
+                response, 'appointment-table-%s' % i)
+
+        # only one new appointment link should appear, since the "today"
+        # panel shouldn't get one
+        new_appointment_links = re.findall(
+            re.escape('href="/appointment/new?date=') +
+            r'[0-9]{4}-[0-9]{2}-[0-9]{2}',
+            response.content)
+
+        self.assertEqual(len(new_appointment_links), 1)
+
+        # one 'mark as noshow' link should appear, since the the today
+        # panel has two pts
+        noshow_links = re.findall(
+            re.escape('href="/appointment/') +
+            r'[0-9]+' +
+            re.escape('/noshow'),
+            response.content)
+
+        self.assertEqual(len(noshow_links), 1)
+
+        # one 'mark as here' link should appear, since the the today
+        # panel has two pts
+        arrived_links = re.findall(
+            re.escape('href="/appointment/') +
+            r'[0-9]+' +
+            re.escape('/arrived'),
+            response.content)
+
+        self.assertEqual(len(arrived_links), 1)
+
