2e62272c03b4afd4e778db0272445654f43caa7f
Moses Chung <kchung25@wustl.edu>
2020-05-05 00:25:29 -0500

    Added DrugCategory and Formulation class. Modified Drugs class. Used ForeignKey.

--- inventory/models.py
+++ inventory/models.py
@@ -6,56 +6,32 @@ from django.db import models
 # Create your models here.
 
 
-class drugs(models.Model):
+class DrugCategory(models.Model):
 
-    name = models.CharField(max_length=100, blank=False)        # name of drug
-
-    dose = models.IntegerField()
-
-    total_inventory = models.IntegerField()
-
-    choices = (
-        ('Available', 'Item ready to be dispensed'),
-        ('Out of stock', 'Item currently depleted'),
-        ('Restocking', 'Item being restocked')
-    )
-    # Available, Out of Stock, Restocking
-    status = models.CharField(max_length=20, default="Out of Stock")
-
-    class Meta:
-        abstract = True
+    name = models.CharField(max_length=100, blank=False, primary_key=True)
 
     def __str__(self):
-        return 'Name: {0} Dose: {1}'.format(self.name, self.dose)
+        return '{}'.format(self.name)
 
 
-class Supplements(drugs):
-    pass
+class Formulation(models.Model):
+
+    dose_mg = models.DecimalField(
+        max_digits=10, decimal_places=4, null=True, blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.dose_mg)
 
 
-class Allergy(drugs):
-    pass
+class Drugs(models.Model):
 
+    name = models.CharField(max_length=100, blank=False)
 
-class Cardiovascular(drugs):
-    pass
+    total_inventory = models.IntegerField(default=0, null=True, blank=True)
 
+    category = models.ForeignKey(DrugCategory)
 
-class Pain_Relief(drugs):
-    pass
+    dose = models.ForeignKey(Formulation)
 
-
-class Antibiotics(drugs):
-    pass
-
-
-class Endocrine_and_GI(drugs):
-    pass
-
-
-class Respiratory(drugs):
-    pass
-
-
-class Other(drugs):
-    pass
+    def __str__(self):
+        return 'Category: {0} Name: {1} Dose: {2}'.format(self.category, self.name, self.dose)
