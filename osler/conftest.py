c01aedd6b7b63fdc6da5bd1f31d43dbec484f337
Justin R. Porter <justinrporter@gmail.com>
2020-05-08 12:52:19 -0500

    Cookiecutter init.

---
+++ osler/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from osler.users.models import User
+from osler.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
