ad0a564308e05d868f168f59e509b6b4135bbcd9
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-10-29 18:47:37 -0500

    write_once for patient, remove destroy, no put

--- osler/workup/api/serializers.py
+++ osler/workup/api/serializers.py
@@ -8,15 +8,37 @@ class ClinicDateSerializer(serializers.ModelSerializer):
         exclude = []
 
 
+
+#to make certain fields write_once, aka only specified on post and read-only on put
+#https://stackoverflow.com/questions/16522845/write-once-read-only-field
+#http://blog.qax.io/write-once-fields-with-django-rest-framework/
+
+#either need to use get extra kwargs or write a separate serializer :(
+#i think id rather use kwargs just cuz then have to figure out type of request and then supply specific serializer
+
 class WorkupSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Workup
-        # fields = ['chief_complaint', 'clinic_day', 'pk', 'url', 'signer']
+        #fields = ['chief_complaint', 'clinic_day', 'pk', 'url', 'signer', 'patient', 'author_id']
         exclude = []
 
     clinic_day = ClinicDateSerializer()
     url = serializers.StringRelatedField(read_only=True)
     signer = serializers.StringRelatedField(read_only=True)
+
+    #to request just specific fields
+    #ex: /api/workups/id/?fields=clinic_day,chief_complaint  for an individual workup
+    # or /api/workups/?fields=clinic_day,chief_complaint  for list view 
+    def __init__(self, *args, **kwargs):
+        super(WorkupSerializer, self).__init__(*args, **kwargs)
+
+        fields = self.context['request'].query_params.get('fields')
+        if fields:
+            fields = fields.split(',')
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
 
     def create(self, validated_data):
         #These fields all needed to be popped because they require foreign keys or many to many relationships.
@@ -33,13 +55,29 @@ class WorkupSerializer(serializers.ModelSerializer):
         workup = models.Workup.objects.create(**validated_data, clinic_day=clinic_instance)
         return workup
 
-    def update(self, instance, validated_data):
-        # other_volunteer = validated_data.pop('other_volunteer')
-        # diagonsis_categories = validated_data.pop('diagnosis_categories')
-        # referral_type = validated_data.pop('referral_type')
-        # referral_location = validated_data.pop('referral_location')
-        # clinic_day = validated_data.pop('clinic_day')
-        # clinic_instance, created = models.ClinicDate.objects.get_or_create(clinic_date=clinic_day['clinic_date'],clinic_type=clinic_day['clinic_type'])
-        if serializer.is_valid():
-            serializer.save()
-        return serializer
+    # def update(self, instance, validated_data):
+    #     read_only_fields = ['patient',]
+    #     # other_volunteer = validated_data.pop('other_volunteer')
+    #     # diagonsis_categories = validated_data.pop('diagnosis_categories')
+    #     # referral_type = validated_data.pop('referral_type')
+    #     # referral_location = validated_data.pop('referral_location')
+    #     # clinic_day = validated_data.pop('clinic_day')
+    #     # clinic_instance, created = models.ClinicDate.objects.get_or_create(clinic_date=clinic_day['clinic_date'],clinic_type=clinic_day['clinic_type'])
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #     return serializer
+
+    #need to do, probably similar to form validation
+    def validate(self, data):
+        return data
+
+    def get_extra_kwargs(self):
+        extra_kwargs = super(WorkupSerializer, self).get_extra_kwargs()
+        action = self.context['view'].action
+
+        if action in ['update', 'partial_update']:
+            kwargs = extra_kwargs.get('patient', {})
+            kwargs['read_only'] = True
+            extra_kwargs['patient'] = kwargs
+
+        return extra_kwargs
--- osler/workup/api/views.py
+++ osler/workup/api/views.py
@@ -2,13 +2,14 @@ from rest_framework import status
 # from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from .serializers import WorkupSerializer
 
 from osler.workup.models import Workup, ClinicDate
 
-class WorkupViewSet(ModelViewSet):
+class WorkupViewSet(CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,ListModelMixin,GenericViewSet):
     serializer_class = WorkupSerializer
     queryset = Workup.objects.all()
 
