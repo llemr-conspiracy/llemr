a4e4ec7fcdb742399f32ffc76cc19f0cc9bd15ae
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-11-05 15:40:05 -0600

    small put update

--- osler/workup/api/serializers.py
+++ osler/workup/api/serializers.py
@@ -55,29 +55,38 @@ class WorkupSerializer(serializers.ModelSerializer):
         workup = models.Workup.objects.create(**validated_data, clinic_day=clinic_instance)
         return workup
 
-    # def update(self, instance, validated_data):
-    #     read_only_fields = ['patient',]
-    #     # other_volunteer = validated_data.pop('other_volunteer')
-    #     # diagonsis_categories = validated_data.pop('diagnosis_categories')
-    #     # referral_type = validated_data.pop('referral_type')
-    #     # referral_location = validated_data.pop('referral_location')
-    #     # clinic_day = validated_data.pop('clinic_day')
-    #     # clinic_instance, created = models.ClinicDate.objects.get_or_create(clinic_date=clinic_day['clinic_date'],clinic_type=clinic_day['clinic_type'])
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #     return serializer
+    def update(self, instance, validated_data):
+        read_only_fields = ['patient',]
+        # other_volunteer = validated_data.pop('other_volunteer')
+        # diagonsis_categories = validated_data.pop('diagnosis_categories')
+        # referral_type = validated_data.pop('referral_type')
+        # referral_location = validated_data.pop('referral_location')
+        # clinic_day = validated_data.pop('clinic_day')
+        # clinic_instance, created = models.ClinicDate.objects.get_or_create(clinic_date=clinic_day['clinic_date'],clinic_type=clinic_day['clinic_type'])
+        other_volunteer = validated_data.pop('other_volunteer')
+        diagonsis_categories = validated_data.pop('diagnosis_categories')
+        referral_type = validated_data.pop('referral_type')
+        referral_location = validated_data.pop('referral_location')
+        clinic_day = validated_data.pop('clinic_day')
+        instance.pmh = validated_data['pmh']
+        instance.psh = validated_data['psh']
+        instance.save()
+        return instance
 
     #need to do, probably similar to form validation
-    def validate(self, data):
-        return data
+    # def validate(self, data):
+    #     return data
 
     def get_extra_kwargs(self):
         extra_kwargs = super(WorkupSerializer, self).get_extra_kwargs()
         action = self.context['view'].action
 
+        #how to do clinic_day since it is a nested object
+        read_only_fields = ['patient','written_datetime','author','author_type']
         if action in ['update', 'partial_update']:
-            kwargs = extra_kwargs.get('patient', {})
-            kwargs['read_only'] = True
-            extra_kwargs['patient'] = kwargs
+            for field_name in read_only_fields:
+                kwargs = extra_kwargs.get(field_name, {})
+                kwargs['read_only'] = True
+                extra_kwargs[field_name] = kwargs
 
         return extra_kwargs
