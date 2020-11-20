53dece71cfd30471b13719ceb9ee849bb999ab31
sebastianapw <19513409+sebastianapw@users.noreply.github.com>
2020-11-20 23:25:16 +0100

    added styling to filter-bar

--- osler/datadashboard/urls.py
+++ osler/datadashboard/urls.py
@@ -1,19 +1,49 @@
 from __future__ import unicode_literals
-from django.urls import re_path
+from django.urls import re_path, path
+
+from django.views.i18n import JavaScriptCatalog
 
 from osler.core.urls import wrap_url
 from osler.datadashboard import views
 
+<<<<<<< Updated upstream
+=======
+
+# from django.urls import include, path
+# from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register(r'workups', views.WorkupViewSet, basename='user')
+# urlpatterns = router.urls
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+# # urlpatterns = [
+# #     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# # ]
+
+js_info_dict = {
+    'packages': ('datadashboard',),
+}
+
+>>>>>>> Stashed changes
 app_name = 'datadashboard'
 unwrapped_urlconf = [
     re_path(
         r'^$',
+<<<<<<< Updated upstream
         views.display_hypertensive,
         name='display-hypertensive'),
     re_path(
         r'^diabetes/$',
         views.display_diabetes,
         name='display-diabetes'),
+=======
+        views.DataDashboardView.as_view(),
+        name='patient-data-dashboard'),
+    re_path(r'^send-json/', views.send_json, name='send_json'),
+    re_path(r'^jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog')
+>>>>>>> Stashed changes
 ]
 
 wrap_config = {}
