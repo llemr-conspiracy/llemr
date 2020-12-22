64140833a390c8a1add2b55a8108c08d04eca38f
ericsallinger <eric.sallinger303@gmail.com>
2020-12-22 15:47:09 -0700

    fix some merge errors

--- osler/workup/forms.py
+++ osler/workup/forms.py
@@ -31,9 +31,9 @@ def form_required_if(form, conditional, fields):
         for f in fields:
             val = data.get(f)
             if val is None or val == '':
-                err_str = (_("When %s is specified, %s must also be specified") %
+                err_str = ("When %s is specified, %s must also be specified" %
                            (conditional, ", ".join(fields)))
-                err_str += (_(" (%s wasn't).") % f) if len(fields) > 1 else "."
+                err_str += (" (%s wasn't)." % f) if len(fields) > 1 else "."
 
                 form.add_error(conditional, err_str)
                 [form.add_error(err_fld, err_str) for err_fld in fields]
@@ -51,7 +51,8 @@ def form_require_together(form, fields):
             if data.get(field) is None:
                 form.add_error(
                     field,
-                    _("The fields %s are required together, and %s was not provided.") % (fields, field))
+                    "The fields %s are required together, and %s was "
+                    "not provided." % (fields, field))
 
 
 def fahrenheit2centigrade(f):
@@ -161,7 +162,7 @@ class WorkupForm(ModelForm):
         required=False,
         queryset=get_user_model().objects.filter(
             groups__in=Group.objects.filter(
-            permissions__codename=can_sign_perm_codename)
+                permissions__codename=can_sign_perm_codename)
         ).distinct()
     )
 
@@ -177,16 +178,15 @@ class WorkupForm(ModelForm):
         self.helper.form_method = 'post'
 
         self.helper.layout = Layout(
-            Row(HTML("<h3>%s</h3>" % _('Clinical Team')),
+            Row(HTML('<h3>Clinical Team</h3>'),
                 Div('attending', css_class='col-sm-6'),
                 Div('other_volunteer', css_class='col-sm-6'),
                 Div('clinic_day', css_class='col-sm-12')
                 ),
 
-            Row(HTML("<h3>%s</h3>" % ('History')),
+            Row(HTML('<h3>History</h3>'),
                 Div('chief_complaint', css_class='col-sm-6'),
                 Div('diagnosis', css_class='col-sm-6'),
-                Div('diagnosis_icd10', css_class='col-xs-12', onkeyup="searchStates(search)"),
                 Div(InlineCheckboxes('diagnosis_categories'),
                     css_class='col-sm-12'),
                 Div('hpi', css_class='col-xs-12'),
@@ -198,8 +198,8 @@ class WorkupForm(ModelForm):
                 Div('allergies', css_class='col-md-6'),
                 Div('ros', css_class='col-xs-12')),
 
-            Row(HTML("<h3>%s</h3>" % _('Physical Exam')),
-                HTML("<h4>%s</h4>" % _('Vital Signs')),
+            Row(HTML('<h3>Physical Exam</h3>'),
+                HTML('<h4>Vital Signs</h4>'),
                 Div(AppendedText('bp_sys', 'mmHg'),
                     css_class='col-md-3 col-sm-3 col-xs-6'),
                 Div(AppendedText('bp_dia', 'mmHg'),
@@ -217,22 +217,25 @@ class WorkupForm(ModelForm):
                     css_class='col-md-3 col-sm-4 col-xs-6'),
                 Div('pe', css_class='col-xs-12')),
 
-            Row(HTML("<h3>%s</h3>" % _("Assessment, Plan, & Orders")),
+            Row(HTML('<h3>Assessment, Plan, & Orders</h3>'),
                 Div('a_and_p', css_class='col-xs-12'),
                 Div('rx', css_class='col-md-4'),
                 Div('labs_ordered_internal', css_class='col-md-4'),
-                Div('labs_ordered_quest', css_class='col-md-4'),
-                Div(HTML("<h4>%s</h4>" % _('Medication Voucher')),
-                    'got_voucher',
-                    PrependedText('voucher_amount', _('$')),
-                    PrependedText('patient_pays', _('$')),
-                    css_class='col-xs-6',),
-                Div(HTML("<h4>%s</h4>" % _('Imaging Voucher')),
-                    'got_imaging_voucher',
-                    PrependedText('imaging_voucher_amount', _('$')),
-                    PrependedText('patient_pays_imaging', _('$')),
-                    css_class='col-xs-6')
+                Div('labs_ordered_external', css_class='col-md-4')
                 ),
+
+            Row(
+                Div(HTML('<h4>Medication Voucher</h4>'),
+                    'got_voucher',
+                    PrependedText('voucher_amount', '$'),
+                    PrependedText('patient_pays', '$'),
+                    css_class='col-xs-6',),
+                Div(HTML('<h4>Imaging Voucher</h4>'),
+                    'got_imaging_voucher',
+                    PrependedText('imaging_voucher_amount', '$'),
+                    PrependedText('patient_pays_imaging', '$'),
+                    css_class='col-xs-6')
+            ),
 
             Submit('pending', _('Save for Later'), css_class='btn btn-warning'),
             Submit('complete', _('Submit'), css_class='btn btn-success')
@@ -252,7 +255,6 @@ class WorkupForm(ModelForm):
             self.helper.layout[5].pop()
             # # delete imaging voucher
             self.helper.layout[5].pop()
-            
 
     def clean(self):
         """Use form's clean hook to verify that fields in Workup are
@@ -266,8 +268,8 @@ class WorkupForm(ModelForm):
             return
 
         required_fields = [
-            'chief_complaint', 
-            'hpi', 
+            'chief_complaint',
+            'hpi',
             'pmh',
             'psh',
             'meds',
@@ -277,7 +279,7 @@ class WorkupForm(ModelForm):
             'ros',
             'pe',
             'a_and_p'
-            ]
+        ]
         for field in required_fields:
             if not cleaned_data.get(field):
                 self.add_error(field, _("This field is required."))
@@ -297,9 +299,9 @@ class WorkupForm(ModelForm):
 
         attending = cleaned_data.get('attending')
         if attending and attending in cleaned_data.get('other_volunteer'):
-            self.add_error('other_volunteer', 
-            'Attending physician must be different from other volunteers.')
-        
+            self.add_error('other_volunteer',
+                           'Attending physician must be different from other volunteers.')
+
         if 't' in cleaned_data and cleaned_data.get('t') is not None:
             if cleaned_data.get('temperature_units') == 'F':
                 c = Decimal(fahrenheit2centigrade(
@@ -326,14 +328,15 @@ class WorkupForm(ModelForm):
             cleaned_data[field] = user_input.strip()
             if "UPDATE" in cleaned_data.get(field):
                 self.add_error(field, _("Please delete the heading and update contents as necessary"))
-        
+
         form_require_together(self, ['bp_sys', 'bp_dia'])
         if cleaned_data.get('bp_sys') and cleaned_data.get('bp_dia'):
             if cleaned_data.get('bp_sys') <= cleaned_data.get('bp_dia'):
                 for field in ['bp_sys', 'bp_dia']:
                     self.add_error(
                         field,
-                        _('Systolic blood pressure must be strictly greater than diastolic blood pressure.'))
+                        'Systolic blood pressure must be strictly greater '
+                        'than diastolic blood pressure.')
 
 
 class AttestableBasicNoteForm(ModelForm):
@@ -344,7 +347,7 @@ class AttestableBasicNoteForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(AttestableBasicNoteForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Submit('submit', _('Submit')))
 
 
 class BasicNoteForm(ModelForm):
