a1e097babf6c43f478ef62e61f6412ceb7c0e8ae
wwick <wwick@wustl.edu>
2021-01-24 11:17:02 -0800

    space between rows now

--- osler/assets/workup/workup-detail/WorkupDetail.js
+++ osler/assets/workup/workup-detail/WorkupDetail.js
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Accordion from 'react-bootstrap/Accordion';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import CardDeck from 'react-bootstrap/CardDeck';
+
+
 import WorkupSection from './WorkupSection';
+
 
 
 function WorkupDetail(props) {
 
     const [loading, setLoading] = useState(true);
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState({
+        at_a_glance: [],
+        h_and_p: []
+    });
 
     useEffect(() => {
         const apiUrl = "/api/workups/" + props.pk;
@@ -19,11 +29,11 @@ function WorkupDetail(props) {
                 const data = response.data;
                 console.log(data);
 
-                setItems({
+                const sections = {
                     at_a_glance: [
                         {
                             name: 'Chief Complaint',
-                            value: data.cc,
+                            value: data.chief_complaint,
                         },
                         {
                             name: 'Author',
@@ -80,7 +90,17 @@ function WorkupDetail(props) {
                             value: data.ros,
                         },
                     ],
-                });
+                };
+
+
+                for (let i = 0; i < sections.at_a_glance.length; i++) {
+                    if (i % 3 == 0) {
+                        items.at_a_glance.push([]);
+                    }
+                    let j = Math.floor(i / 3);
+                    items.at_a_glance[j].push(sections.at_a_glance[i]);
+                }
+
 
                 setLoading(false);
             });
@@ -96,15 +116,34 @@ function WorkupDetail(props) {
             <Row>
                 <h4>{'At a Glance'}</h4>
             </Row>
-            <Row>
-                <WorkupSection items={items.at_a_glance}/>
+            {items.at_a_glance.map((row) => (
+            <Row className='mb-3'>
+                {row.map((field) => (
+                    <Col md={4} key={field.name}>
+                        <Card key={field.name} className='h-100'>
+                            <Card.Header>{field.name}</Card.Header>
+                            <Card.Body>{field.value}</Card.Body>
+                        </Card>
+                    </Col>
+                ))}
             </Row>
-            <Row>
+            ))}
+
+            {/* <Row>
                 <h4>{'H and P'}</h4>
             </Row>
             <Row>
-                <WorkupSection items={items.hp}/>
-            </Row>
+                <Col md={12} key='h_and_p'>
+                    <Accordion>
+                    {items.hp.map((item) => (
+                        <Card key={item.name}>
+                            <Card.Header>{item.name}</Card.Header>
+                            <Card.Body>{item.value}</Card.Body>
+                        </Card>
+                    ))}
+                    </Accordion>
+                </Col>
+            </Row> */}
             </>
             )}
         </Container>
--- osler/assets/workup/workup-detail/WorkupSection.js
+++ osler/assets/workup/workup-detail/WorkupSection.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 
 function WorkupSection(items) {
 
     return (
+        <Col md={12}>
         <Accordion>
         {items.items.map((item) => (
             <Card key={item.name}>
@@ -14,6 +16,7 @@ function WorkupSection(items) {
             </Card>
         ))}
         </Accordion>
+        </Col>
     );
 }
 
