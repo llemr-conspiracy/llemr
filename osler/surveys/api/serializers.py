d4c94d589b72b675fb76592db1a61e10f112fbb4
Brian Jubelirer <brian2386@gmail.com>
2021-03-02 20:03:39 -0500

    add question_type to questions api

--- osler/surveys/api/serializers.py
+++ osler/surveys/api/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 from osler.surveys.models import Survey, Question, Choice
 
+
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ['id', 'text']
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     choices = ChoiceSerializer(read_only=False, many=True)
 
     class Meta:
         model = Question
-        fields = ['id','survey', 'question', 'choices']
+        fields = ['id', 'survey', 'question', 'choices', 'question_type']
+
 
 class SurveySerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(read_only=True, many=True)
