c4ee946f09033868b73ca0bd65fb74ab6932775a
sebastianapw <19513409+sebastianapw@users.noreply.github.com>
2021-03-14 21:43:08 +0100

    Nginx and production environment settings for stupoli

---
+++ config/settings/stupoli.py
@@ -0,0 +1,33 @@
+# Custom settings for stupoli deploy
+# -----------------------------------------------------------------------------
+
+OSLER_ROLE_DASHBOARDS = {
+    'Attending': 'dashboard-attending',
+    'Physician': 'dashboard-attending',
+}
+
+OSLER_DISPLAY_REFERRALS = False
+OSLER_DISPLAY_APPOINTMENTS = False
+OSLER_DISPLAY_CASE_MANAGERS = False
+OSLER_DISPLAY_ATTESTABLE_BASIC_NOTE = False
+OSLER_DISPLAY_DIAGNOSIS = False
+OSLER_DISPLAY_VOUCHERS = False
+OSLER_DISPLAY_WILL_RETURN = False
+OSLER_DISPLAY_ATTENDANCE = True
+OSLER_DISPLAY_FOLLOWUP = False
+OSLER_DISPLAY_VACCINE = False
+
+OSLER_DEFAULT_CITY = "Hamburg"
+OSLER_DEFAULT_STATE = "Hamburg"
+OSLER_DEFAULT_ZIP_CODE = "20459"
+OSLER_DEFAULT_COUNTRY = "Deutschland"
+OSLER_DEFAULT_ADDRESS = "Seewartenstraße 10"
+
+OSLER_ABOUT_NAME = "wiki.stupoli-hamburg.de"
+
+# EMAIL-Settings
+# EMAIL_HOST =
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
+# EMAIL_PORT =
+# EMAIL_USE_TLS =
