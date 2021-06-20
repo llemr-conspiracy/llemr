347a7b96c76ff6281e26f7b8b648d20c44429a8d
wwick <wwick@wustl.edu>
2021-06-19 21:47:19 -0700

    simplify middleware

--- config/settings/local-stupoli.py
+++ config/settings/local-stupoli.py
@@ -5,19 +5,7 @@ from .stupoli import *
 # in development. allow it in production in case someone wants
 # to somehow specify a different language 
 # https://docs.djangoproject.com/en/3.2/topics/i18n/translation/#how-django-discovers-language-preference
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'simple_history.middleware.HistoryRequestMiddleware',
-    'osler.audit.middleware.AuditMiddleware'
-]
+MIDDLEWARE.remove('django.middleware.locale.LocaleMiddleware')
 
 # uncomment below to enable English
 # LANGUAGE_CODE = "en-us"
