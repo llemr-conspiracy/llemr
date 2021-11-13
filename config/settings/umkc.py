ecc1368b7f755eac50c06616ae66747ab4b0ff61
wwick <wwick@wustl.edu>
2021-11-12 20:49:11 -0600

    umkc email config

--- config/settings/umkc.py
+++ config/settings/umkc.py
@@ -7,6 +7,15 @@ OSLER_ROLE_DASHBOARDS = {
     'Physician': 'dashboard-attending',
 }
 
+# overrides ./.envs/.production/.django to add mystery IP address
+ALLOWED_HOSTS = ['osler.umkc.edu', '134.193.143.63']
+
+DEFAULT_FROM_EMAIL = SERVER_EMAIL = 'remote.med.umkc.edu@umkc.edu'
+EMAIL_HOST = 'massmail.umsystem.edu'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+
 OSLER_DISPLAY_REFERRALS = False
 OSLER_DISPLAY_APPOINTMENTS = False
 OSLER_DISPLAY_CASE_MANAGERS = False
@@ -26,7 +35,3 @@ OSLER_DEFAULT_ADDRESS = "3151 Olive St"
 
 OSLER_ABOUT_NAME = "Help"
 OSLER_ABOUT_URL = "http://www.sojournerhealthclinic.com/?page_id=1755"
-
-# Default Encounter Status
-OSLER_DEFAULT_ACTIVE_STATUS = ('Present', True)
-OSLER_DEFAULT_INACTIVE_STATUS = ('Inactive', False)
