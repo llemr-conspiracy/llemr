946d2e353f5996f773ebac7c8a8569497b9b11a4
wwick <wwick@wustl.edu>
2022-02-22 13:17:45 -0600

    load charts.js through cdn, remove large fixtures

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./osler/assets/datadashboard/index.js", // path to our input file
-  output: {
-    filename: "datadashboard-imports.bundle.js", // output bundle file name
-    path: path.resolve(__dirname, "./osler/static/js/datadashboard"), // path to our Django static directory
-  },
-};
